9b962a570ffc9b1ccb4870145cc0a6205d5d1583
Nils Daumann <nils_daumann@slindev.com>
2016-08-19 13:28:15 +0200

    Empty files for control scheme screen.

---
+++ game/source/ControlMenu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nils Daumann on 19.08.16.
+//
+
+#include "ControlMenu.h"
---
+++ game/source/ControlMenu.h
@@ -0,0 +1,17 @@
+//
+// Created by Nils Daumann on 19.08.16.
+//
+
+#ifndef FANCYGOLDENLAMPS_CONTROLMENU_H
+#define FANCYGOLDENLAMPS_CONTROLMENU_H
+
+namespace FGL
+{
+	class ControlMenu
+	{
+	public:
+
+	};
+}
+
+#endif //FANCYGOLDENLAMPS_CONTROLMENU_H
